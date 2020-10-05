9b1e7fe091060d24e32da8681812daa4d1bcdfae
t179a <67852630+t179a@users.noreply.github.com>
2020-10-05 23:52:35 +0900

    Add files via upload

---
+++ ch06/07-proxy-a/test.js
@@ -0,0 +1,20 @@
+'use stirct'
+
+const createProxy = require('./createProxy');
+
+class Greeter {
+  hello(){
+    return 'Hello';
+  }
+
+  goodbye(){
+    return 'Goodbye';
+  }
+}
+
+const greeter = new Greeter();
+
+const proxy = createProxy(greeter);
+
+console.log(proxy.hello());
+console.log(proxy.goodbye());
