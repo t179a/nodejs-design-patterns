47b1c6ea7abc0c59a5d5de38f4a8304faa024971
t179a <67852630+t179a@users.noreply.github.com>
2020-10-07 21:02:30 +0900

    Add files via upload

---
+++ ch06/17-state/client.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const createFailsafeSocket = require('./failsafeSocket');
+
+const failsafeSocket = createFailsafeSocket({port: 5000});
+
+setInterval(() => {
+  failsafeSocket.send(process.memoryUsage());
+}, 1000)
---
+++ ch06/17-state/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const jot = require('json-over-tcp');
+
+const server = jot.createServer();
+
+server.on('connection', socket => {
+  socket.on('data', data => {
+    console.log('Client data', data);
+  });
+});
+
+server.listen(5000, () => console.log('Started'));
