af11e1284110286ef771da350610c3243ce21603
t179a <67852630+t179a@users.noreply.github.com>
2020-10-12 00:49:45 +0900

    Add files via upload

---
+++ ch07/03a-service-locator/populate_db.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const sublevel = require('level-sublevel');
+const level = require('level');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const async = require('async');
+
+const db = sublevel(level('exapmle-db', {valueEncoding: 'json'}));
+const usersDb = db.sublevel('users');
+
+const users = [
+  {username: 'alice', password: 'secret'},
+  {username: 'bob', password: 'secret'},
+  {username: 'trudy', password: 'secret'}
+];
+
+async.forEach(users, (user, callback) => {
+  usersDb.put(user.username, {
+    hash: bcrypt.hashSync(user.password, 8)
+  }, callback);
+}, (err) => {
+  if(err) {
+    return console.log(err);
+  }
+  console.log('DB populated');
+});
