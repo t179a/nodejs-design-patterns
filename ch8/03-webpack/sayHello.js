ba26314a91c6058069e8b3f117142bad3be31636
t179a <67852630+t179a@users.noreply.github.com>
2020-10-13 06:02:21 +0900

    Add files via upload

---
+++ ch8/03-webpack/sayHello.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const mustache = require('mustache');
+const template = '<h1>Hello <i>{{name}}</i></h1>';
+mustache.parse(template);
+module.exports.sayHello = function(toWhom){
+  return mustache.render(template, {name: toWhom});
+};
+
